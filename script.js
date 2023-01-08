fafe51418d3f6739ee112bb93388c9a013a56261
cmsjadmin <121534103+cmsjadmin@users.noreply.github.com>
2023-01-07 18:46:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function displayFiles(response) {
             
             <li data-id="${gdapifiles[i].id}" data-name="${gdapifiles[i].id}">
             <span>
-                <a href="${gdapifiles[i].webViewLink}" target="_blank">"${gdapifiles[i].name}"</a>
+                <a href="${gdapifiles[i].webViewLink}">"${gdapifiles[i].name}"</a>
             </span>
             </li>
             
